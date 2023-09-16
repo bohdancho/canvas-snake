a4c867430d59287147bf1577489c99972d5ca502
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-09-16 12:01:37 +0200

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-expressions': 'warn',
+  },
+}
