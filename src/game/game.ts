54d768cef5b54e746b00b71f054ac5a08ce3fe64
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-09-16 10:58:07 +0200

    add reset, ditch old vite styles, add size to game

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,14 +1,24 @@
 export class Game {
+  private width: number
+  private height: number
   private ctx: CanvasRenderingContext2D
   constructor(canvas: HTMLCanvasElement) {
+    this.ctx = Game.getContext(canvas)
+
+    const { width, height } = canvas.getBoundingClientRect()
+    this.width = width
+    this.height = height
+  }
+  public init() {
+    this.ctx.fillStyle = 'green'
+    this.ctx.fillRect(0, 0, this.width, this.height)
+  }
+
+  private static getContext(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext('2d')
     if (!ctx) {
       throw Error('No ctx')
     }
-    this.ctx = ctx
-  }
-  public init() {
-    this.ctx.fillStyle = 'green'
-    this.ctx.fillRect(10, 10, 150, 100)
+    return ctx
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
-import './default.css'
+import 'reset-css'
 import { Game } from './game/game'
 import './styles.css'
 
-function init() {
-  const canvas = document.querySelector<HTMLCanvasElement>('#canvas')
-  if (!canvas) {
-    throw Error('No canvas')
-  }
-
-  const game = new Game(canvas)
-  game.init()
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')
+if (!canvas) {
+  throw Error('No canvas')
 }
 
-init()
+const game = new Game(canvas)
+game.init()
